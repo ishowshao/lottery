04e9b44c3a4d391b7e196398a778a6b2eca63f16
ishowshao <ishowshao@me.com>
2021-01-22 20:15:34 +0800

    dev

---
+++ src/views/Admin.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>ADMIN</h1>
+    <div style="display: flex">
+      <div style="width: 40vw">
+        <div>设置</div>
+        <el-form size="small" label-width="150px">
+          <el-form-item label="本轮抽取">
+            <el-input-number v-model="count"></el-input-number>位
+          </el-form-item>
+          <el-form-item label="允许重复中奖">
+            <el-switch v-model="repeat"></el-switch>
+          </el-form-item>
+          <el-form-item label="页面背景图">
+            <el-input v-model="backgroundImage"></el-input>
+          </el-form-item>
+          <el-form-item label="抽奖按钮文本1">
+            <el-input v-model="buttonText"></el-input>
+          </el-form-item>
+          <el-form-item label="抽奖按钮文本2">
+            <el-input v-model="buttonText2"></el-input>
+          </el-form-item>
+          <el-form-item label="抽奖按钮图片">
+            <el-input v-model="buttonImage" placeholder="请填写图片URL"></el-input>
+          </el-form-item>
+          <el-form-item label="抽奖按钮字体大小">
+            <el-input-number v-model="buttonFontSize"></el-input-number>
+          </el-form-item>
+          <el-form-item label="">
+            <el-button @click="save">保存</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div style="flex: 1">
+        <div>中奖信息</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      repeat: false,
+      backgroundImage: '',
+      buttonText: '开始',
+      buttonText2: '结束',
+      buttonImage: '',
+      buttonFontSize: 16,
+    };
+  },
+  methods: {
+    save() {
+      console.log(JSON.stringify(this.$data))
+      localStorage.setItem('config', JSON.stringify(this.$data));
+    }
+  },
+  mounted() {
+    const config = JSON.parse(localStorage.getItem('config') || '{}');
+    Object.assign(this.$data, config);
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" :style="{'background-image': `url(${backgroundImage})`}">
+    <div class="winners">
+      <div v-for="(name, index) in names" :key="index">{{ name }}</div>
+    </div>
+    <div>
+      <div class="button" :style="{'background-image': `url(${buttonImage})`, 'font-size': `${buttonFontSize}px`}">{{buttonText}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      names: ["???", "???"],
+      count: 0,
+      repeat: false,
+      backgroundImage: '',
+      buttonText: '开始',
+      buttonImage: '',
+      buttonFontSize: 16,
+    };
+  },
+  mounted() {
+    const config = JSON.parse(localStorage.getItem('config')) || {};
+    console.log(config);
+    Object.assign(this.$data, config);
+    window.addEventListener('storage', () => {
+      // When local storage changes, dump the list to
+      // the console.
+      const config = JSON.parse(localStorage.getItem('config')) || {};
+      Object.assign(this.$data, config);
+    });
+  },
+};
 </script>
+<style scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+}
+.winners {
+  display: flex;
+}
+.button {
+  position: absolute;
+  background-size: cover;
+}
+</style>
